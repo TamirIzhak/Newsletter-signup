1a683a5f77925ee3ab62b56955531aefa616d2ad
Tamir Izhak <tamir.izhak1987@gmail.com>
2020-01-02 17:54:52 +0200

    some custome changes in the html and added logo

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require ("request");
-
 const app = express();
+
 app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(3000, function(){
     console.log("Server is up in port 3000");
