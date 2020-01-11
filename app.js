69d9e46cfb91ceb8a5393a3b62ca81284bdf7c0a
Tamir Izhak <tamir.izhak1987@gmail.com>
2020-01-11 17:55:43 +0200

    added failure and success pages

--- app.js
+++ app.js
@@ -1,25 +1,63 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require ("request");
+const request = require("request");
 const app = express();
 
 app.use(express.static(__dirname));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.listen(3000, function(){
+app.listen(3000, function() {
     console.log("Server is up in port 3000");
 });
 
-app.get("/", function(req,res){
+app.get("/", function(req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req,res){
+
+app.post("/", function(req, res) {
 
     var firstName = req.body.firstName;
     var lastName = req.body.lastName;
     var email = req.body.email;
+    var data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    };
 
-    console.log(" "+firstName+" "+lastName+" "+email);
+    var options = {
+        url: "https://us4.api.mailchimp.com/3.0/lists/7cd165f022",
+        method: "POST",
+        headers: {
+            "Authorization": "basic 503501a55f2c45061c349f7546ec37e3-us4"
+        },
+        body: [JSON.stringify(data)]
+    };
+
+    request(options, function(error, response, body) {
+        if (error) {
+            res.sendFile(__dirname+"/failure.html");
+        } else if (response.statusCode === 200) {
+            res.sendFile(__dirname+"/success.html");
+        } else {
+            res.sendFile(__dirname+"/failure.html");
+        }
+    });
 });
+
+app.post("/failure", function(req, send){
+
+    res.redirect("/");
+
+});
+// 503501a55f2c45061c349f7546ec37e3-us4 // - mailchimp api
+// 7cd165f022 // -  Armadillomail list ID
